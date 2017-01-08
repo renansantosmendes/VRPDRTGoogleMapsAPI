5a5492cc04d2c654662ab302a892d106cb23cdf7
renansantos <renansantos@renansantos-Lenovo-G480>
2017-01-08 13:42:23 -0200

    Commit Test

--- NetBeansProjects/mavenproject2/src/main/java/VRPDRTSD/Main.java
+++ NetBeansProjects/mavenproject2/src/main/java/VRPDRTSD/Main.java
@@ -6,38 +6,11 @@
 package VRPDRTSD;
 
 import Algorithms.BuildGreedySolution;
-import VRPDRTSD.IntanceReaderWithMySQL.AdjacenciesDataAcessObject;
-import VRPDRTSD.IntanceReaderWithMySQL.NodeDataAcessObject;
-import VRPDRTSD.IntanceReaderWithMySQL.RequestDataAcessObject;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import javax.imageio.ImageIO;
-import jdk.internal.org.xml.sax.XMLReader;
-import org.joda.time.LocalDateTime;
-import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.xml.sax.helpers.XMLReaderFactory;
+
 
 /**
  *
