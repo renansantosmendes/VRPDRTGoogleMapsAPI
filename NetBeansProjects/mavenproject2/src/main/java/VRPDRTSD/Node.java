11296b4fc01c60876f9177902b599e1feae14548
renansantos <renansantos@renansantos-Lenovo-G480>
2017-01-25 23:21:47 -0200

    Alterações feitas nas classes: Request e Node
    Request - foi inserido alguns dos critérios iniciais para avaliar a solicitação em relação ao seu atendimento. Esses critérios serão utilizados para construir uma solução gulosa ou aleatória.
    
    Node - foi adicionado um atributo na classe, representando o índice de lotação (número de passageiros que embarcam menos o número de passageiros que desembarcam). O métodos equal e hashCode foram adicionados para tentar comparar os nós. Ainda não estão funcionando

--- NetBeansProjects/mavenproject2/src/main/java/VRPDRTSD/Node.java
+++ NetBeansProjects/mavenproject2/src/main/java/VRPDRTSD/Node.java
@@ -9,7 +9,7 @@ public class Node {
     private final Integer nodeId;
     private final Double longitude;
     private final Double latitude;
-    
+    private int loadIndex;
        
     /**
      * 
@@ -21,6 +21,7 @@ public class Node {
         this.nodeId = nodeId;
         this.longitude = longitude;
         this.latitude = latitude;
+        this.loadIndex = 0;
     }
     
     /**
@@ -45,10 +46,46 @@ public class Node {
      */
     public Double getLatitude() {
         return latitude;
-    }    
+    }
+    
+     /**
+     * 
+     * @return int - returns the node load index: number of passenger which boards
+     * in this node minus the number of passenger which leaves the vehicle in this
+     * node
+     */
+    public int getLoadIndex() {
+        return loadIndex;
+    }
+
+    public void setLoadIndex(int loadIndex){
+        this.loadIndex = loadIndex;
+    }
+    
     
     @Override
     public String toString(){
         return "Node(" + this.nodeId +") " + "Lat = " + this.latitude + " Long = " + this.longitude;
+    }
+    
+    @Override
+    public boolean equals(Object obj){
+       return obj instanceof Node && equals((Node) obj);
+    }
+    
+    public boolean equals(Node node){
+        if(this.getNodeId() == node.getNodeId()){
+            if(this.getLatitude() == node.getLatitude() && this.getLongitude() == node.getLongitude()){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public int hashCode(){
+        String string = Integer.toString(this.getNodeId());
+        int hash = string.hashCode();
+        return hash;
     }
 }
